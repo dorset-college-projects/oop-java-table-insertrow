d0b1e06cc10bdffe6f0fc6734596be08d5f7104d
John Rowley <outsidethebox@eircom.net>
2021-02-23 13:26:28 +0000

    added columns

--- src/com/dorsetcollegeoop/Main.java
+++ src/com/dorsetcollegeoop/Main.java
@@ -12,14 +12,18 @@ public class Main {
 
         JTable table = new JTable(tableModel);
 
-        tableModel.addColumn("Languages");
+      //  tableModel.addColumn("Languages");
+       // tableModel.addColumn("Type");
 
-        tableModel.insertRow(0,new Object[]{"CSS"});
-        tableModel.insertRow(0,new Object[]{"HTML5"});
-        tableModel.insertRow(0,new Object[]{"JavaScript"});
-        tableModel.insertRow(0,new Object[]{"Jquery"});
-        tableModel.insertRow(0,new Object[]{"Angular"});
-        tableModel.insertRow(tableModel.getRowCount(),new Object[]{"Node"});
+        tableModel.setColumnIdentifiers(new String[]{"Language", "Type"});
+
+        tableModel.insertRow(0,new Object[]{"CSS","FRONTEND"});
+        tableModel.insertRow(0,new Object[]{"HTML5","FRONTEND"});
+        tableModel.insertRow(0,new Object[]{"C#","SERVERSIDE"});
+        tableModel.insertRow(0,new Object[]{"JavaScript","FRONTEND"});
+        tableModel.insertRow(0,new Object[]{"Jquery","FRONTEND"});
+        tableModel.insertRow(0,new Object[]{"Angular","FRONTEND"});
+        tableModel.insertRow(tableModel.getRowCount(),new Object[]{"Node","BOTH"});
 
         JFrame f = new JFrame();
         f.setSize(550,350);
